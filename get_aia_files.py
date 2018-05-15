946a2212948268201020038fabde15519da500ae
jprchlik <japrchlik@gmail.com>
2018-05-15 16:06:05 -0400

    Final version of script to get AIA files

--- get_aia_files.py
+++ get_aia_files.py
@@ -8,7 +8,10 @@ def wrap_get_files(args):
 
 def get_files(stime,etime,archive = 'aia_arch/'):
     fobj = adf.download_files(stime,etime,['193','304','335'],'30m',email='jakub.prchlik@cfa.harvard.edu',odir=archive)
-    fobj.get_drms_files()
+    try:
+        fobj.get_drms_files()
+    except:
+        return
 
 def format_dates(data):
     x = data.str.replace('-','/')
@@ -17,14 +20,20 @@ def format_dates(data):
 
     return x
 
+#read in csv file
 aia = pd.read_csv('Sigmoids2007to2017.csv')
 
-#aia = aia.loc[50:54,:]
 
+#format start times and end times
 aia['fmt_stime'] = format_dates(aia.SIG_START)
 aia['fmt_etime'] = format_dates(aia.SIG_END)
 
 
+#remove rows without times
+aia.SIG_START.fillna('',inplace=True)
+aia.loc[aia.SIG_START != '',:]
+
+#create list for parallel processing
 inpt =  aia[['fmt_stime','fmt_etime']].values.tolist()
 
 
